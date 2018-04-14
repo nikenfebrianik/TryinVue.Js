b4d705a1511155049a68c06bf695e041df287245
Niken Febriani Kusumawati <nikenfebrianik@gmail.com>
2018-04-14 11:25:17 +0700

    mencoba

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="form">
     <input v-model="msg" />
+    <button v-on:click="addMessage">Add</button>
     <br />
+    <ul>
+      <li v-for="(message, id) in listMessage" :key="id">{{message}}</li>
+    </ul>
     <label>{{msg}}</label>
   </div>
 </template>
@@ -10,7 +14,14 @@
     name: "Message Form",
       data(){
         return{
+          listMessage: [],
           msg: "",
+        }
+      },
+      methods: {
+        addMessage(){
+          this.listMessage.push(this.msg);
+          this.msg = "";
         }
       }
   }
