a95f6449b62aa193b2845d61b71b2bfdfd3dbbbc
Niken Febriani Kusumawati <nikenfebrianik@gmail.com>
2018-04-14 11:08:49 +0700

    perbaikan

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <message-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageForm from './components/MessageForm'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MessageForm
   }
 }
 </script>
--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,2 +1,17 @@
 <template>
   <div class="form">
+    <input v-model="msg" />
+    <br />
+    <label>{{msg}}</label>
+  </div>
+</template>
+<script>
+  export  default {
+    name: "Message Form",
+      data(){
+        return{
+          msg: "",
+        }
+      }
+  }
+</script>
